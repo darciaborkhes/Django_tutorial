a9594a49f2da3dbd388c2bbeec7ba0669473c170
darinusia <d.khomyk@gmail.com>
2018-09-27 14:58:59 +0200

    Squashed commit of the following:
    commit c19323c732a9d24eee706be088f8e301854e50b2
    Author: darinusia <d.khomyk@gmail.com>
    Date:   Fri Sep 7 15:10:11 2018 +0200

--- students/urls.py
+++ students/urls.py
@@ -4,8 +4,11 @@ from django.contrib import admin
 
 app_name = 'students'
 urlpatterns = [
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    url(r'^$', views.students_view, name = 'student_list' ),
+    url(r'^(?P<pk>[0-9]+)/$', views.StudentDetailView.as_view(), name='detail'),
+    url(r'^add$', views.StudentCreateView.as_view(), name='student_add'),
+    url(r'^edit/(?P<pk>[0-9]+)/$', views.StudentUpdateView.as_view(), name='student_edit'),
+    url(r'^remove/(?P<pk>[0-9]+)/$', views.StudentDeleteView.as_view(), name='student_edit'),
+    url(r'$', views.StudentListView.as_view(), name = 'student_list' ),
 
 
 ]
--- students/views.py
+++ students/views.py
@@ -1,25 +1,79 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from .models import Student
+from django.views.generic.list import ListView
+from .forms import StudentModelForm
+from django.contrib import messages
+from django.urls import reverse_lazy
 
+class StudentListView(ListView):
+
+    model = Student
+    context_object_name = 'students_list'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        course_id = self.request.GET.get('course_id', '')
+        if course_id:
+            qs = qs.filter(courses__id=course_id)
+        return qs
 
 
 
 
 # Create your views here.
-def students_view(request):
+# def students_view(request):
+#
+#     course_id = request.GET.get('course_id','')
+#     if course_id:
+#         students_list = Student.objects.filter(courses__id=course_id)
+#
+#     else:
+#         students_list = Student.objects.all()
+#
+#
+#     return render(request, 'students/student_list.html', {'students_list':students_list})
 
-    course_id = request.GET.get('course_id','')
-    if course_id:
-        students_list = Student.objects.filter(courses__id=course_id)
-
-    else:
-        students_list = Student.objects.all()
-
-
-    return render(request, 'students/student_list.html', {'students_list':students_list})
-
-class DetailView(generic.DetailView):
+class StudentDetailView(DetailView):
     context_object_name = 'student'
     model = Student
     template_name = 'students/detail.html'
+
+class StudentCreateView(CreateView):
+    model = Student
+    form_class = StudentModelForm
+    template_name = 'students/student_add.html'
+    success_url = reverse_lazy('students:student_list')
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        course_id = self.request.GET.get('course_id', '')
+        if course_id:
+            qs = qs.filter(courses__id=course_id)
+        return qs
+
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        form.save()
+        messages.success(self.request, 'The student has been added')
+        return response
+
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    form_class = StudentModelForm
+    template_name = 'students/student_edit.html'
+    success_url = reverse_lazy('students:student_list')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        form.save()
+        messages.success(self.request, 'The student has been saved')
+        return response
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    form_class = StudentModelForm
+    template_name = 'students/student_remove.html'
+    success_url = reverse_lazy('students:student_list')
